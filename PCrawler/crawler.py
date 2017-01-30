62a5669b08c67e178de8f9de437a43f385bf37f1
xumenger-mac <xumenger@126.com>
2017-01-30 21:17:21 +0800

    爬虫框架

--- PCrawler/crawler.py
+++ PCrawler/crawler.py
@@ -2,12 +2,21 @@
 
 class Crawler(object):
     # URL下载方法
+    def download:
+        pass
 
     # URL管理方法
+    def manage:
+        pass
 
     # HTML解析方法
+    def parse:
+        pass
 
     # 解析内容输出(存储方法)
+    def output:
+        pass
 
     # 按照配置的线程/进程、按照实现的方法运行爬虫
-
+    def Execute:
+        pass
