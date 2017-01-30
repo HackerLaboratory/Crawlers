669d397c60cd73e76fa7b7ac368ecf0bc652f2b9
xumenger-mac <xumenger@126.com>
2017-01-30 21:19:11 +0800

    方法()

--- PCrawler/crawler.py
+++ PCrawler/crawler.py
@@ -2,21 +2,21 @@
 
 class Crawler(object):
     # URL下载方法
-    def download:
+    def download():
         pass
 
     # URL管理方法
-    def manage:
+    def manage():
         pass
 
     # HTML解析方法
-    def parse:
+    def parse():
         pass
 
     # 解析内容输出(存储方法)
-    def output:
+    def output():
         pass
 
     # 按照配置的线程/进程、按照实现的方法运行爬虫
-    def Execute:
+    def Execute():
         pass
