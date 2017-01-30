a415242a9439f13798a3b94e939ca6257c846f33
xumenger-mac <xumenger@126.com>
2017-01-30 21:24:40 +0800

    可配置

--- PCrawler/config.py
+++ PCrawler/config.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
 #多线程 or 多进程
+isMultiProcess = True
 
 #下载器线程/进程数量
+downloader = 2
 
 #解析器进程/线程个数
+parser = 2
 
 #URL管理器
+manager = 2
 
 #数据存储器进程/线程个数
+outputer = 2
 
 #匹配的URL正则表达式
+reURL = ''
 
-#开始爬取的正则表达式
-
+#开始爬取的URL
+startURL = 'http://www.xumenger.com'
