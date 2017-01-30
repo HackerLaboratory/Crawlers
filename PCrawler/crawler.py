6dcca288d582608d06973ab88fb375015804ddc3
xumenger-mac <xumenger@126.com>
2017-01-30 22:45:18 +0800

    实现url管理器

--- PCrawler/crawler.py
+++ PCrawler/crawler.py
@@ -24,14 +24,33 @@ class Crawler(object):
 
     def __init__(self):
         print 'init'
+        self.new_urls = set()       #新的还未被爬取的url集合，使用集合进行管理
+        self.old_urls = set()       #已经被爬取过的url集合，也是使用集合进行管理
 
     # URL下载方法
     def download(self):
         print 'download'
 
-    # URL管理方法
-    def manage(self):
-        print 'manage'
+    # 添加一个新的url
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if (url not in self.new_urls) and (url not in self.old_urls):
+            self.new_urls.add(url)
+    # 添加批量url
+    def add_new_urls(self, urls):
+        if (urls is None) or (0 == len(urls)):
+            return
+        for url in urls:
+            self.add_new_url(url)
+    # 判断是否还有未爬取的url
+    def has_new_url(self):
+        return (0 != len(self.new_urls))
+    # 获取一个新的待爬取的url
+    def get_new_url(self):
+        new_url = self.new_urls.pop()   #pop方法是从集合中获取一个元素，并将其中集合中移除
+        self.old_urls.add(new_url)
+        return new_url
 
     # HTML解析方法
     def parse(self):
