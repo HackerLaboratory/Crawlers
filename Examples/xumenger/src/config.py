5d0565e0abaa014317484601d1c741f4b4e8b8e0
xumenger <xumenger@126.com>
2017-02-22 23:56:24 +0800

    支持同时爬取多个网站

--- Examples/xumenger/src/config.py
+++ Examples/xumenger/src/config.py
@@ -12,10 +12,10 @@ parseCount = 2
 #数据存储进程/线程个数
 outputCount = 3
 
-#匹配的URL正则表达式
+#匹配的URL正则表达式，以及对应的处理类的类名
 reURLs = {'http://www.xumenger.com/.*/': 'xumenger', 
           'http://www.xumenger.com/page.*': 'page'}
 
-#开始爬取的URL
+#开始爬取的URL，支持配置多个URL，可用于同时抓取多个网站
 startURLs = ['http://www.xumenger.com']
 
